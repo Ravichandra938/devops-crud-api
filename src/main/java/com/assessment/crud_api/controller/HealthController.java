214692ea5d1913e1e527819157926f66a54539e9
Ravi Chandra Mouli Kommu <93654397+Ravichandra938@users.noreply.github.com>
2026-07-09 12:54:24 +0530

    Update HealthController.java

--- src/main/java/com/assessment/crud_api/controller/HealthController.java
+++ src/main/java/com/assessment/crud_api/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.assessment.crudapi.controller;
+package com.assessment.crud_api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
