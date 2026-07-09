b5678ce2488b5662d1c955ef18618d6cf8451e9f
Ravi Chandra Mouli Kommu <93654397+Ravichandra938@users.noreply.github.com>
2026-07-09 13:00:19 +0530

    Update TaskRepository.java

--- src/main/java/com/assessment/crud_api/repository/TaskRepository.java
+++ src/main/java/com/assessment/crud_api/repository/TaskRepository.java
@@ -1,6 +1,6 @@
-package com.assessment.crudapi.repository;
+package com.assessment.crud_api.repository;
 
-import com.assessment.crudapi.model.DeploymentTask;
+import com.assessment.crud_api.model.DeploymentTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<DeploymentTask, Long> {}
